ec5fa2d4e524f06b501d7054f1d731dcdb598f1b
Nicolas <nicolas.maurin.33@gmail.com>
2019-11-04 15:09:44 +0100

    Integration Flutter

---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+// Used for controlling whether the user is loggin or creating an account
+enum FormType { login, register }
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailFilter = new TextEditingController();
+  final TextEditingController _passwordFilter = new TextEditingController();
+  String _email = "";
+  String _password = "";
+  FormType _form = FormType
+      .login; // our default setting is to login, and we should switch to creating an account when the user chooses to
+
+  _LoginPageState() {
+    _emailFilter.addListener(_emailListen);
+    _passwordFilter.addListener(_passwordListen);
+  }
+
+  void _emailListen() {
+    if (_emailFilter.text.isEmpty) {
+      _email = "";
+    } else {
+      _email = _emailFilter.text;
+    }
+  }
+
+  void _passwordListen() {
+    if (_passwordFilter.text.isEmpty) {
+      _password = "";
+    } else {
+      _password = _passwordFilter.text;
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/8f91b942c680648023d75b40093fd8d5.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: EdgeInsets.all(66.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildTextFields(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextFields() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            child: new Text(
+              "Bienvenue sur l'application",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
+          ),
+          SizedBox(
+              height: 155.0,
+              child:
+              Image.asset("assets/madera_logo.png", fit: BoxFit.contain)),
+          new Container(
+            child: new TextField(
+              textAlign: TextAlign.center,
+              controller: _emailFilter,
+              decoration: new InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(color: Colors.white),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 3.0),
+                    borderRadius: BorderRadius.circular(62.00)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(62.00)),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          new Container(
+            child: new TextField(
+              controller: _passwordFilter,
+              decoration: new InputDecoration(
+                labelText: 'Mot de passe',
+                labelStyle: TextStyle(color: Colors.white),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 3.0),
+                    borderRadius: BorderRadius.circular(62.00)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(62.00)),
+              ),
+              obscureText: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)
+            ),
+            child: new Text('Se connecter',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            onPressed: _loginPressed,
+          ),
+          new FlatButton(
+            child: new Text(
+              'Mot de passe oublié ?',
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            onPressed: _passwordReset,
+          )
+        ],
+      ),
+    );
+  }
+
+  // These functions can self contain any user auth logic required, they all have access to _email and _password
+
+  void _loginPressed() {
+    print('The user wants to login with $_email and $_password');
+  }
+
+  void _passwordReset() {
+    print("The user wants a password reset request sent to $_email");
+  }
+}
